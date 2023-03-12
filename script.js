8d6ce0e2888138184d5f722717913c73147a4589
PITERKE <piotrdziula@gmail.com>
2023-03-12 21:26:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+function getGBP(){
+    const url = "http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json";
+
+    return a = axios.get(url)
+    .then(function (response) {
+        j = response.data
+        return j;
+        
+    })
+
+}
+
+function setRate(){
+    getGBP().then(function(){
+        document.getElementById('rate').innerText = j.rates[0].mid;
+        document.getElementById('GBP').value = 1.00;
+        document.getElementById('PLN').value = 1.00 * j.rates[0].mid;
+    });
+}
+
+function isNumeric(value){
+    return /^[0-9]+(\.[0-9]+)?$/.test(value);
+}
+
+
+function convertCurrencies(currency, value){
+    id  = currency == 'GBP' ? 'PLN' :  'GBP';
+    
+    if(isNumeric(value)){
+        
+        rate = document.getElementById('rate').innerText;
+        document.getElementById(id).value = (id == "PLN" ? (value * rate).toFixed(2) : (value / rate).toFixed(2));
+    
+    }    
+    else {
+        document.getElementById(id).value = "";
+    }    
+
+}
+
+
+
+
+
+
+
